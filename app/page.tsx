d12675f7225599c0c30330cd584d8c69ac832a61
clodi <ciao@clodi.tech>
2024-09-17 18:52:37 +0200

    fetch graphql

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <div>Hello World</div>;
+const query = `
+query {
+  football{
+    allCards{
+      nodes{
+        age
+      }
+    }
+  }
+}
+  `;
+
+export default async function Home() {
+  const response = await fetch("https://api.sorare.com/federation/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query }),
+    cache: "no-store",
+  });
+
+  const { data } = await response.json();
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      {JSON.stringify(data)}
+    </div>
+  );
 }
